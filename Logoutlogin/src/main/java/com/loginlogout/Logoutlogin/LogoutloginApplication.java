3eacdbf378a7a8cbea1057d9ded1c9a48ba56b85
Moizsaleem078 <moizsaleem28@gmail.com>
2022-05-09 14:39:52 +0500

    das

--- Logoutlogin/src/main/java/com/loginlogout/Logoutlogin/LogoutloginApplication.java
+++ Logoutlogin/src/main/java/com/loginlogout/Logoutlogin/LogoutloginApplication.java
@@ -7,7 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class LogoutloginApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(LogoutloginApplication.class, args);
+		//SpringApplication.run(LogoutloginApplication.class, args);
+		System.out.println("sdasd");
 		myclass m=new myclass();
 	    myclass.getmy();
 	}
